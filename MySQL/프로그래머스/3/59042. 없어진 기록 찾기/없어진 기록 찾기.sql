1a40abc3457e2bbaa685d553b15e525629dbf195
Guk Hyun <moonstalker9010@gmail.com>
2025-11-29 17:42:53 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ MySQL/프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -0,0 +1,10 @@
+# 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회
+
+SELECT
+    O.ANIMAL_ID,
+    O.NAME
+FROM ANIMAL_OUTS AS O # 입양을 보낸 TABLE
+LEFT OUTER JOIN ANIMAL_INS AS I # 보호소에 들어온 TABLE
+    ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL # 조건문
+ORDER BY I.ANIMAL_ID;
