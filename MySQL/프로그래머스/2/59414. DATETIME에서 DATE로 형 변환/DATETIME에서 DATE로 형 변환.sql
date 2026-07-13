beded1c1b7fd189ec1eee0a5edcb46d286c0c9ee
Guk Hyun <moonstalker9010@gmail.com>
2026-07-13 22:07:32 +0900

    [level 2] Title: DATETIME에서 DATE로 형 변환, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/59414. DATETIME에서 DATE로 형 변환/DATETIME에서 DATE로 형 변환.sql
@@ -0,0 +1,7 @@
+-- 모든 레코드에 대해, 각 동물의 아이디와 이름, 들어온 날짜1를 조회하는 SQL문을 작성해주세요. 이때 결과는 아이디 순으로 조회
+SELECT
+    ANIMAL_ID,
+    NAME,
+    DATE(DATETIME) AS 날짜 # DATE = 시간 부분만 잘라냄 / DATE_FORMAT(DATETIME, '%Y-%m-%d') AS 날짜 = 이렇게도 가능
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
